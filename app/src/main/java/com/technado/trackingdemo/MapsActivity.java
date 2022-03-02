b4bb709b462a9dc56aae15f497477ed7fb7573e5
asad.muqeem <raoasad125@gmail.com>
2022-03-02 18:31:53 +0500

    Tracking Complete

--- app/src/main/java/com/technado/trackingdemo/MapsActivity.java
+++ app/src/main/java/com/technado/trackingdemo/MapsActivity.java
@@ -12,6 +12,7 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
 import android.widget.Toast;
@@ -60,8 +61,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
 
         locationRequest = LocationRequest.create();
-        locationRequest.setInterval(500);
-        locationRequest.setFastestInterval(500);
+        locationRequest.setInterval(5000);
+        locationRequest.setFastestInterval(3000);
         locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
         dbRef = FirebaseDatabase.getInstance().getReference("Lat_Lng");
